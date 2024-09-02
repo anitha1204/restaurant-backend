2ac3f0e4b5a33932744dff80eabf8f7bae17fa25
anitha1204 <selvam12042003@gmail.com>
2024-09-02 15:52:26 +0530

    main

--- model/userModel.js
+++ model/userModel.js
@@ -1,3 +1,28 @@
+// const mongoose = require("mongoose");
+// const user = new mongoose.Schema({
+//   userName: {
+//     type: String,
+//   },
+//   phonNumber: {
+//     type: Number,
+//   },
+//   email: {
+//     type: String,
+//   },
+//   location: {
+//     type: String,
+//   },
+//   date:{
+//     type: String,
+//   }
+
+// });
+
+// const userDB = mongoose.model("ani", user);
+// module.exports = userDB;
+
+
+
 const mongoose = require("mongoose");
 const user = new mongoose.Schema({
   userName: {
